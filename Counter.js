7fd97b689823d16d3da0a6d6922c3a041cedf13d
Priyanshuchaudhry2004 <priyanschaudhry01@gmail.com>
2026-09-06 23:12:45 +0530

    Add files via upload

--- Counter.js
+++ Counter.js
@@ -1,1 +1,12 @@
+var createCounter = function(n) {
+    return function() {
+        return n++;
+    };
+};
 
+const counter = createCounter(15);
+
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
+console.log(counter()); // 13
